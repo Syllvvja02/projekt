8b095e1901ee25fc99eaa1d96fac8b86d21078e4
Syllvvja02 <mikolajczykdrazeksylwia@gmail.com>
2022-10-21 11:23:49 +0200

    zrobione

--- zadania.py
+++ zadania.py
@@ -1,6 +1,9 @@
 # 1. Napisz funkcję, która przyjmie wiek osoby, a następnie zwróci informację o tym, czy jest pełnoletnia czy nie.
 def check_adult(age):
-    pass
+    if age < 18:
+        print("Niepełnoletni")
+    else:
+        print("Pełnoletni")
 
 
 check_adult(34) # Pełnoletni
@@ -9,16 +12,22 @@ check_adult(17) # Niepełnoletni
 
 # 2. Napisz funkcję, która zwraca listę wszystkich podzielnych przez n liczb z zakresu [1, 100].
 def divisible(n):
-    pass
+    lst = []
+    for x in range(1, 101):
+        if x % n == 0:
+            lst.append(x)
+        else:
+            continue
+    print(lst)
 
 
 divisible(3) # [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99]
 divisible(5) # [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 divisible(8) # [8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96]
 
-
+#
 # 3. Napisz funkcję, która przyjmuje kwotę (value) i jej walutę (currency), a następnie zwróci ile wart jest dana kwota w danej walucie w złotówkach.
-[
+lst_of_dct = [
     {
         "currency": "USD",
         "value": 4.56
@@ -32,7 +41,9 @@ divisible(8) # [8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96]
 ]
 
 def check_currency(value, currency):
-    pass
+    for x in lst_of_dct:
+        if x["currency"] == currency:
+            print(f'{round(value * x["value"], 2)} zł')
 
 
 check_currency(745, "CHF") # 3747,35 zł
